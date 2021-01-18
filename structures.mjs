23f29f359ab0195599677a8e84c5c16d249386f2
Lokprakash-babu <lokprakash100@gmail.com>
2021-01-18 22:16:00 +0530

    changed exports

--- structures.mjs
+++ structures.mjs
@@ -1,97 +1,105 @@
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+class BinaryTree{
 
-export class BinaryTree {
-  constructor(value) {
-    _defineProperty(this, "addLeft", node => {
-      if (node instanceof BinaryTree) {
-        this.left = node;
-      } else {
-        throw 'Data type mismatch';
-      }
-    });
-
-    _defineProperty(this, "addRight", node => {
-      if (node instanceof BinaryTree) {
-        this.right = node;
-      } else {
-        throw 'Data type mismatch';
-      }
-    });
-
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-
+    constructor(value){
+        this.value=value;
+        this.left=null;
+        this.right=null;
+    }
+    addLeft=(node)=>{
+        if(node instanceof BinaryTree){
+            this.left=node;
+        }
+        else{
+            throw 'Data type mismatch'
+        }
+    }
+    addRight=(node)=>{
+        if(node instanceof BinaryTree){
+            this.right=node;
+        }
+        else{
+            throw 'Data type mismatch'
+        }
+    }
 }
-export class Stack {
-  constructor() {
-    _defineProperty(this, "pushValue", val => {
-      this.elements.push(val);
-      this.top = this.elements[this.elements.length - 1];
-    });
 
-    _defineProperty(this, "popValue", () => {
-      let value = this.elements.pop();
+class Stack{
+    constructor(){
+        this.elements=[];
+        this.top=null;
+    }
 
-      if (this.elements.length == 0) {
-        this.top = null;
-      } else {
-        this.top = this.elements[this.elements.length - 1];
-      }
+    pushValue=(val)=>{
+        this.elements.push(val);
+        this.top=this.elements[this.elements.length-1];
+    }
 
-      return value;
-    });
+    popValue=()=>{
+        let value = this.elements.pop();
+        if(this.elements.length==0){
+            this.top=null;
+        }
+        else{
+            this.top=this.elements[this.elements.length-1];
+        }
+        return value;
+    }
 
-    _defineProperty(this, "getTop", () => {
-      return this.top;
-    });
+    getTop=()=>{
+        return this.top;
+    }
 
-    _defineProperty(this, "getLength", () => {
-      return this.elements.length;
-    });
-
-    this.elements = [];
-    this.top = null;
-  }
-
+    getLength=()=>{
+        return this.elements.length;
+    }
 }
-export class Queue {
-  constructor() {
-    _defineProperty(this, "pushValue", value => {
-      this.elements.push(value);
-    });
 
-    _defineProperty(this, "popValue", () => {
-      let value = this.elements[0];
-      this.elements.shift();
-      return value;
-    });
+class Queue{
+    constructor(){
+        this.elements=[];
+        this.top=null;
+    }
 
-    _defineProperty(this, "getTop", () => {
-      return this.elements[0];
-    });
+    pushValue=(value)=>{
+        this.elements.push(value);
+    }
 
-    _defineProperty(this, "getLength", () => {
-      return this.elements.length;
-    });
+    popValue=()=>{
+        let value=this.elements[0];
+        this.elements.shift();
+        return value;
+    }
 
-    this.elements = [];
-    this.top = null;
-  }
+    getTop=()=>{
+        return this.elements[0];
+    }
 
+    getLength=()=>{
+        return this.elements.length;
+    }
 }
-export class LinkedList {
-  constructor(value) {
-    _defineProperty(this, "addNext", node => {
-      if (node instanceof LinkedList) {
-        this.next = node;
-      } else {
-        throw "Data type mismatch";
-      }
-    });
 
-    this.value = value;
-  }
+class LinkedList{
+    constructor(value){
+        this.value=value;
+        
+    }
 
+
+
+    addNext=(node)=>{
+        if(node instanceof LinkedList){
+            this.next=node;
+        }
+        else{
+            throw "Data type mismatch";
+        }
+    }
 }
+
+module.exports={
+    BinaryTree: BinaryTree,
+    Stack: Stack,
+    Queue: Queue,
+    LinkedList: LinkedList
+};
